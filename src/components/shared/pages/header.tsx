7e9b4670507c63ce053a664fadeafffe77cc2c8c
Kip West <kip@numero.ai>
2025-03-08 17:10:56 -0800

    set up responsive styles

--- src/components/shared/pages/header.tsx
+++ src/components/shared/pages/header.tsx
@@ -6,34 +6,53 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
 
 export function Header() {
   return (
     <header className={classNames("bg-gray-900 text-white", "p-3")}>
       <div className="container mx-auto">
-        <NavigationMenu
-          className={classNames("max-w-full flex justify-between")}
-        >
+        <NavigationMenu className="max-w-full flex justify-between">
           <NavigationMenuLink
             href="/"
             className={classNames("text-xl font-bold")}
           >
             Dani Strauss
           </NavigationMenuLink>
-
-          <NavigationMenuList className={classNames("flex gap-3")}>
-            {Object.entries(PAGES_CONFIG)
-              .filter(([key]) => key !== "index")
-              .map(([key, value]) => (
-                <NavigationMenuItem key={key}>
-                  <NavigationMenuLink href={value.href}>
-                    {value.title}
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-              ))}
-          </NavigationMenuList>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" className="lg:hidden">
+                |||<span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="top">
+              <PagesLinks />
+            </SheetContent>
+          </Sheet>
+          <div className={classNames("hidden lg:flex")}>
+            <PagesLinks />
+          </div>
         </NavigationMenu>
       </div>
     </header>
+  );
+}
+
+function PagesLinks() {
+  return (
+    <NavigationMenuList
+      className={classNames("flex flex-col lg:flex-row gap-3")}
+    >
+      {Object.entries(PAGES_CONFIG)
+        .filter(([key]) => key !== "index")
+        .map(([key, value]) => (
+          <NavigationMenuItem key={key}>
+            <NavigationMenuLink href={value.href}>
+              {value.title}
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+    </NavigationMenuList>
   );
 }
--- src/components/shared/pages/main.tsx
+++ src/components/shared/pages/main.tsx
@@ -6,11 +6,9 @@ type MainProps = PropsWithChildren<{ title: string }>;
 
 export function Main({ children, title }: MainProps) {
   return (
-    <main className="flex-grow">
-      <div className={classNames("container mx-auto", "p-2")}>
-        <h1 className={"text-2xl font-bold"}>{title}</h1>
-        <ScrollArea>{children}</ScrollArea>
-      </div>
+    <main className={classNames("container mx-auto", "flex-grow", "p-2")}>
+      <span className="sr-only">{title} Page</span>
+      <ScrollArea>{children}</ScrollArea>
     </main>
   );
 }
